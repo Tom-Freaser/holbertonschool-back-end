e3407879137fee42a12c255018eb43ab0dc21e1e
root <root@yassin>
2023-12-21 16:02:56 +0100

    api

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-"""Script to export data in the CSV format."""
+"""import"""
 import csv
 import requests
-from sys import argv
+import sys
 
-API_URL = 'https://jsonplaceholder.typicode.com'
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(f"missing employee id as argument")
+        sys.exit(1)
 
+    URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
 
-if __name__ == '__main__':
-    USER_ID = argv[1]
+    EMPLOYEE_TODOS = requests.get(f"{URL}/users/{EMPLOYEE_ID}/todos",
+                                  params={"_expand": "user"})
+    data = EMPLOYEE_TODOS.json()
 
-    # User information
-    user_response = requests.get(f"{API_URL}/users/{USER_ID}").json()
+    EMPLOYEE_NAME = data[0]["user"]["username"]
+    fileName = f"{EMPLOYEE_ID}.csv"
 
-    # Todo list for the given user
-    todo_response = requests.get(f"{API_URL}/todos?userId={USER_ID}").json()
-
-    # Write to CSV file
-    with open(f"{USER_ID}.csv", mode='w') as csv_file:
-        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-
-        for task in todo_response:
-            writer.writerow([
-                user_response['id'],
-                user_response['username'],
-                task['completed'],
-                task['title']
-            ])
-
-    print(f"Data has been exported to {USER_ID}.csv")
+    with open(fileName, "w", newline="") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
+        for task in data:
+            writer.writerow(
+                [EMPLOYEE_ID, EMPLOYEE_NAME, str(task["completed"]),
+                 task["title"]]
+            )
--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
-"""Script to export data in the JSON format."""
+"""import"""
 import json
 import requests
-from sys import argv
+import sys
 
-API_URL = 'https://jsonplaceholder.typicode.com'
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(f"missing employee id as argument")
+        sys.exit(1)
 
+    URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
 
-if __name__ == '__main__':
-    USER_ID = argv[1]
+    EMPLOYEE_TODOS = requests.get(f"{URL}/users/{EMPLOYEE_ID}/todos",
+                                  params={"_expand": "user"})
+    data = EMPLOYEE_TODOS.json()
 
-    # User information
-    user_response = requests.get(f"{API_URL}/users/{USER_ID}").json()
-
-    # Todo list for the given user
-    todo_response = requests.get(f"{API_URL}/todos?userId={USER_ID}").json()
-
-    # Prepare data for export
-    data = {
-        USER_ID: [
-            {
-                "task": task['title'],
-                "completed": task['completed'],
-                "username": user_response['username']
-            }
-            for task in todo_response
-        ]
-    }
-
-    # Write to JSON file
-    with open(f"{USER_ID}.json", mode='w') as json_file:
-        json.dump(data, json_file)
-
-    print(f"Data has been exported to {USER_ID}.json")
+    username = data[0]["user"]["username"]
+    USER_TASK = {EMPLOYEE_ID: []}
+    for task in data:
+        dic_task = {"task": task["title"], "completed": task["completed"],
+                    "username": username}
+        USER_TASK[EMPLOYEE_ID].append(dic_task)
+    fileName = f"{EMPLOYEE_ID}.json"
+    with open(fileName, "w") as file:
+        json.dump(USER_TASK, file)
